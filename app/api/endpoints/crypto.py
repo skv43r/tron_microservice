3491e16ab51655967429c2b10c553629efedb991
Alexandr <skv43r@gmail.com>
2025-01-26 18:42:51 +0300

    chore(crypto): wrote documentation

--- app/api/endpoints/crypto.py
+++ app/api/endpoints/crypto.py
@@ -1,3 +1,7 @@
+"""
+Модуль для работы с криптовалютными кошельками.
+"""
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.database import get_db
@@ -6,10 +10,49 @@ from app.services.crypto_service import wallet_info, wallet_history
 
 crypto_router = APIRouter()
 
-@crypto_router.post("/wallet", response_model=WalletResponse, tags=["crypto"], description="Получить информацию о кошельке")
-async def get_wallet_info(wallet_address: str, get_db: AsyncSession = Depends(get_db)):
+
+@crypto_router.post("/wallet",
+                    response_model=WalletResponse,
+                    tags=["crypto"])
+async def get_wallet_info(wallet_address: str,
+                          get_db: AsyncSession = Depends(get_db)):
+    """
+    Получить информацию о криптовалютном кошельке.
+
+    - Параметры:
+        - `wallet_address` (str): Адрес кошелька,
+        информацию о котором нужно получить.
+
+    - Ответ:
+        - Возвращает объект `WalletResponse`, содержащий данные о кошельке,
+          такие как баланс, пропускная способность и энергия.
+
+    - Ошибки:
+        - 404: Ошибка валидации (например, некорректный формат адреса).
+        - 500: Ошибка сервера.
+    """
     return await wallet_info(wallet_address, get_db)
 
-@crypto_router.get("/history", response_model=PaginatedResponse, tags=["crypto"], description="Получить историю кошелька")
-async def get_wallet_history(skip: int = 0, limit: int = 10, get_db: AsyncSession = Depends(get_db)):
+
+@crypto_router.get("/history",
+                   response_model=PaginatedResponse,
+                   tags=["crypto"])
+async def get_wallet_history(skip: int = 0,
+                             limit: int = 10,
+                             get_db: AsyncSession = Depends(get_db)):
+    """
+    Получить историю запростов кошельков.
+
+    - Параметры:
+        - `skip` (int): Количество записей для пропуска (по умолчанию 0).
+        - `limit` (int): Максимальное количество записей для возврата
+          (по умолчанию 10).
+
+    - Ответ:
+        - Возвращает объект `PaginatedResponse`,
+        содержащий общее количество записей и сам список.
+
+    - Ошибки:
+        - 500: Ошибка сервера.
+    """
     return await wallet_history(skip, limit, get_db)
