a1bf8cb9c7b1c8921b9e873545c16505f1e12055
Alexandr <skv43r@gmail.com>
2025-01-26 18:46:04 +0300

    chore(tron): wrote documentation

--- app/core/tron.py
+++ app/core/tron.py
@@ -1,4 +1,10 @@
+"""
+Модуль для настройки Tron клиента.
+"""
+
 from tronpy import Tron
 from tronpy.providers import HTTPProvider
+from app.core.config import settings
 
-tron = Tron(provider=HTTPProvider("https://api.trongrid.io", api_key="096579a1-be7a-43ca-a34b-2c4d8346f92f"))
+tron = Tron(provider=HTTPProvider(settings.ENDPOINT_URI,
+            api_key=settings.API_KEY))
