df502ec128f42705fc70003a3ef701a70ab65fd6
Alexandr <skv43r@gmail.com>
2025-01-26 18:46:20 +0300

    chore(models): wrote documentation

--- app/models/models.py
+++ app/models/models.py
@@ -1,8 +1,17 @@
+"""
+Модель для хранения запросов к кошелькам.
+"""
+
 from sqlalchemy import Column, Integer, String, DateTime
 from app.core.database import Base
 from datetime import datetime
 
+
 class WalletQuery(Base):
+    """
+    Модель для таблицы `wallet_queries`,
+    хранящей информацию о запросах к кошелькам.
+    """
     __tablename__ = "wallet_queries"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
