c0aa7d58a2bdd3684781b7f6a1593cb822ed6b09
Alexandr <skv43r@gmail.com>
2025-01-26 18:44:27 +0300

    refactor(config):  added variables for tests and wrote documentation

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,16 @@
+"""
+Модуль конфигурации.
+"""
+from typing import Literal
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
+    """
+    Класс для хранения настроек.
+    """
+    MODE: Literal["DEV", "TEST"]
+
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
@@ -8,7 +18,21 @@ class Settings(BaseSettings):
     DB_NAME: str
     DB_URL: str
 
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+    TEST_DB_URL: str
+
+    ENDPOINT_URI: str
+    API_KEY: str
+
     class Config:
+        """
+        Конфигурация для класса Settings.
+        """
         env_file = ".env"
+
 
 settings = Settings()
