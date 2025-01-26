cf3b405f5537a87a12435778aade13388b6ee0a2
Alexandr <skv43r@gmail.com>
2025-01-26 18:45:42 +0300

    refactor(database): add mode for tests

--- app/core/database.py
+++ app/core/database.py
@@ -1,14 +1,39 @@
+"""
+Модуль для настройки базы данных и создания сессий.
+"""
+
+from sqlalchemy import NullPool
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from app.core.config import settings
 
-engine = create_async_engine(settings.DB_URL)
+if settings.MODE == "TEST":
+    DB_URL = settings.TEST_DB_URL
+    DB_PARAMS = {"poolclass": NullPool}
+else:
+    DB_URL = settings.DB_URL
+    DB_PARAMS = {}
 
-async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_async_engine(DB_URL, **DB_PARAMS)
+
+async_session_maker = sessionmaker(engine,
+                                   class_=AsyncSession,
+                                   expire_on_commit=False)
+
 
 class Base(DeclarativeBase):
+    """
+    Базовый класс для декларативного определения моделей.
+    """
     pass
 
+
 async def get_db():
+    """
+    Получение асинхронной сессии базы данных.
+
+    - Возвращает:
+        Асинхронную сессию для работы с базой данных.
+    """
     async with async_session_maker() as session:
         yield session
